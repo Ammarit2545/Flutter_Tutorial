033c9d67d8ff3bdb35a2e77c41e32e8707a49176
Arm <armm4sonic@gmail.com>
2022-07-18 10:51:08 +0700

    13th commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,24 +11,38 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "My App",
-      home: Scaffold(
-        appBar: AppBar(
-          //ส่วนหัวแอป
-          title: Text("Hello Flutter "),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text("Hello Dart 1"),
-              Text("Hello Dart 2"),
-              Text("Hello Dart 3")
-            ],
-          ),
+      title: "โปรแกรมนับเลข",
+      home: MyHomePage(),
+      theme: ThemeData(primarySwatch: Colors.green),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //ส่วนหัวแอป
+        title: Text("Hello Flutter "),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text("Hello Dart 1"),
+            Text("Hello Dart 2"),
+            Text("Hello Dart 3")
+          ],
         ),
       ),
-      theme: ThemeData(primarySwatch: Colors.red),
     );
   }
 }
