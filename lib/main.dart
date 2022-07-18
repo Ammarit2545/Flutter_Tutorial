ddfe8bb5f3cedd3fae8813d4fef4be0e0c684eb3
Arm <armm4sonic@gmail.com>
2022-07-18 10:16:47 +0700

    8th commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,14 @@ import 'package:flutter/material.dart';
 void main() {
   var app = MaterialApp(
     title: "My App",
-    home: Text("Hello Dart"),
+    home: Scaffold(
+      appBar: AppBar(
+        //ส่วนหัวแอป
+        title: Text("Hello Flutter "),
+      ),
+      body: Text("Hello Dart"),
+    ),
+    theme: ThemeData(primarySwatch: Colors.green),
   );
   runApp(app);
 }
