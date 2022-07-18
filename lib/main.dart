ce3802f3badd260f6fb5409429bfed4e291d68f1
Arm <armm4sonic@gmail.com>
2022-07-18 10:22:18 +0700

    9th commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  var app = MaterialApp(
-    title: "My App",
-    home: Scaffold(
-      appBar: AppBar(
-        //ส่วนหัวแอป
-        title: Text("Hello Flutter "),
+  runApp(MyApp());
+}
+
+//สร้าง Widget
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My App",
+      home: Scaffold(
+        appBar: AppBar(
+          //ส่วนหัวแอป
+          title: Text("Hello Flutter "),
+        ),
+        body: Text("Hello Dart "),
       ),
-      body: Text("Hello Dart"),
-    ),
-    theme: ThemeData(primarySwatch: Colors.green),
-  );
-  runApp(app);
+      theme: ThemeData(primarySwatch: Colors.green),
+    );
+  }
 }
